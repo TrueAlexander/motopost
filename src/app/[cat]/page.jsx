2a1a136f9a2554d46e00cd6ee02248bec1e9fa31
Alexander <caminante.msk@gmail.com>
2024-07-30 15:48:14 -0300

    try number 4 maybe

--- src/app/[cat]/page.jsx
+++ src/app/[cat]/page.jsx
@@ -19,7 +19,7 @@ const categoryPage = ({searchParams}) => {
 
   return (
   <div className={styles.container}>
-    <h2 className={styles.title} style={{backgroundColor: `${catBg[cat]}`}}>{cat}</h2>
+    <h2 className={styles.title} style={{backgroundColor: `${catBg[cat]}`}}>{"cat"}</h2>
     <div className={styles.content}>
       <CardList page={page} cat={cat}/>
     </div>
